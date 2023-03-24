4d21470f4acd0d8e576e4f45d06d065b7ee4daa8
Nicolas T <nicolas@yapla.be>
2023-03-24 15:25:59 +0100

    🧪 Tests Calendar

---
+++ packages/antwerp-ui/react-components/src/lib/molecules/datepicker/Calendar.spec.tsx
@@ -0,0 +1,167 @@
+import jest from 'jest-mock';
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Calendar } from './Calendar';
+
+describe('UI Components - Molecules - Calendar', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Calendar />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should set the data-qa attribute', () => {
+    const { baseElement } = render(<Calendar qa="calendar-1337" />);
+    expect(
+      baseElement.getElementsByClassName('m-datepicker')[0]?.getAttribute('data-qa') === 'calendar-1337'
+    ).toBeTruthy();
+  });
+
+  it('should call onChange when selecting a date', () => {
+    const mockOnChange = jest.fn();
+    const { baseElement } = render(<Calendar onChange={mockOnChange} />);
+    fireEvent.click(
+      baseElement.getElementsByClassName('m-datepicker__grid-body')[0].querySelector('button') as HTMLButtonElement
+    );
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+
+  it('should call onBlur when focus is removed from Calendar', () => {
+    const mockOnBlur = jest.fn();
+    const { baseElement } = render(<Calendar onBlur={mockOnBlur} />);
+    const focusableElement = baseElement.querySelector('button') as HTMLButtonElement;
+    focusableElement.focus();
+    focusableElement.blur();
+    expect(mockOnBlur).toHaveBeenCalled();
+  });
+
+  it('should have an active date if value is provided', () => {
+    const date = new Date(Date.now()).toISOString();
+    const { baseElement } = render(<Calendar value={date} />);
+    expect(baseElement.getElementsByClassName('is-current').length).toBe(1);
+  });
+
+  it('should rotate through the views', () => {
+    const { baseElement } = render(<Calendar />);
+    const rotateViewButton = baseElement.querySelector('.m-datepicker__nav-title') as HTMLButtonElement;
+    expect(baseElement.getElementsByClassName('m-datepicker__grid-body').length).toBe(1);
+    expect(baseElement.getElementsByClassName('m-datepicker__list').length).toBe(0);
+    fireEvent.click(rotateViewButton);
+    expect(baseElement.getElementsByClassName('m-datepicker__grid-body').length).toBe(0);
+    expect(baseElement.getElementsByClassName('m-datepicker__list').length).toBe(1);
+    fireEvent.click(rotateViewButton);
+    expect(baseElement.getElementsByClassName('m-datepicker__grid-body').length).toBe(0);
+    expect(baseElement.getElementsByClassName('m-datepicker__list').length).toBe(1);
+    fireEvent.click(rotateViewButton);
+    expect(baseElement.getElementsByClassName('m-datepicker__grid-body').length).toBe(1);
+    expect(baseElement.getElementsByClassName('m-datepicker__list').length).toBe(0);
+  });
+
+  it('should change to previous page when clicking on previous button', () => {
+    const date = new Date('2023-02-22').toISOString();
+    const { baseElement, queryByText } = render(<Calendar value={date} />);
+
+    const rotateViewButton = baseElement.querySelector('.m-datepicker__nav-title') as HTMLButtonElement;
+    const prevButton = baseElement.getElementsByClassName('m-datepicker__nav-pagination')[0]
+      .childNodes[0] as HTMLButtonElement;
+
+    // DaysView
+    expect(queryByText('February 2023')).toBeTruthy();
+    fireEvent.click(prevButton);
+    expect(queryByText('February 2023')).toBeFalsy();
+    expect(queryByText('January 2023')).toBeTruthy();
+
+    // MonthsView
+    fireEvent.click(rotateViewButton);
+
+    expect(queryByText('2023')).toBeTruthy();
+    fireEvent.click(prevButton);
+    expect(queryByText('2023')).toBeFalsy();
+    expect(queryByText('2022')).toBeTruthy();
+
+    // YearsView
+    fireEvent.click(rotateViewButton);
+
+    expect(queryByText('2016 - 2033')).toBeTruthy();
+    fireEvent.click(prevButton);
+    expect(queryByText('2016 - 2033')).toBeFalsy();
+    expect(queryByText('1998 - 2015')).toBeTruthy();
+  });
+
+  it('should change to next page when clicking on next button', () => {
+    const date = new Date('2023-02-22').toISOString();
+    const { baseElement, queryByText } = render(<Calendar value={date} />);
+
+    const rotateViewButton = baseElement.querySelector('.m-datepicker__nav-title') as HTMLButtonElement;
+    const nextButton = baseElement.getElementsByClassName('m-datepicker__nav-pagination')[0]
+      .childNodes[1] as HTMLButtonElement;
+
+    // DaysView
+    expect(queryByText('February 2023')).toBeTruthy();
+    fireEvent.click(nextButton);
+    expect(queryByText('February 2023')).toBeFalsy();
+    expect(queryByText('March 2023')).toBeTruthy();
+
+    // MonthsView
+    fireEvent.click(rotateViewButton);
+
+    expect(queryByText('2023')).toBeTruthy();
+    fireEvent.click(nextButton);
+    expect(queryByText('2023')).toBeFalsy();
+    expect(queryByText('2024')).toBeTruthy();
+
+    // YearsView
+    fireEvent.click(rotateViewButton);
+
+    expect(queryByText('2016 - 2033')).toBeTruthy();
+    fireEvent.click(nextButton);
+    expect(queryByText('2016 - 2033')).toBeFalsy();
+    expect(queryByText('2034 - 2051')).toBeTruthy();
+  });
+
+  it('should rotate views correctly when selecting a value in a specific view', () => {
+    const date = new Date('2023-02-22').toISOString();
+    const { baseElement, queryByText } = render(<Calendar value={date} />);
+
+    const rotateViewButton = baseElement.querySelector('.m-datepicker__nav-title') as HTMLButtonElement;
+    // Go to YearsView
+    fireEvent.click(rotateViewButton);
+    fireEvent.click(rotateViewButton);
+    expect(queryByText('2016 - 2033')).toBeTruthy();
+
+    // Select a year and move to MonthsView
+    const yearButton = queryByText('2023') as HTMLButtonElement;
+    fireEvent.click(yearButton);
+    expect(queryByText('2016 - 2033')).toBeFalsy();
+    expect(queryByText('2023')).toBeTruthy();
+
+    // Select a month and move to DaysView
+    const monthButton = queryByText('March') as HTMLButtonElement;
+    fireEvent.click(monthButton);
+    expect(queryByText('2023')).toBeFalsy();
+    expect(queryByText('March 2023')).toBeTruthy();
+  });
+
+  it('should move to previous year if clicking previous on MonthsView in January', () => {
+    const date = new Date('2023-01-22').toISOString();
+    const { baseElement, queryByText } = render(<Calendar value={date} />);
+    const prevButton = baseElement.getElementsByClassName('m-datepicker__nav-pagination')[0]
+      .childNodes[0] as HTMLButtonElement;
+
+    expect(queryByText('January 2023')).toBeTruthy();
+    fireEvent.click(prevButton);
+    expect(queryByText('January 2023')).toBeFalsy();
+    expect(queryByText('December 2022')).toBeTruthy();
+  });
+
+  it('should move to next year if clicking next on MonthsView in December', () => {
+    const date = new Date('2023-12-22').toISOString();
+    const { baseElement, queryByText } = render(<Calendar value={date} />);
+    const nextButton = baseElement.getElementsByClassName('m-datepicker__nav-pagination')[0]
+      .childNodes[1] as HTMLButtonElement;
+
+    expect(queryByText('December 2023')).toBeTruthy();
+    fireEvent.click(nextButton);
+    expect(queryByText('December 2023')).toBeFalsy();
+    expect(queryByText('January 2024')).toBeTruthy();
+  });
+});
---
+++ packages/antwerp-ui/react-components/src/lib/molecules/datepicker/views/CalendarViews.spec.tsx
@@ -0,0 +1,123 @@
+import jest from 'jest-mock';
+import { fireEvent, render } from '@testing-library/react';
+import { DayButton } from './DayButton';
+import { MonthsView } from './MonthsView';
+import { YearsView } from './YearsView';
+
+describe('UI Components - Molecules - Calendar - DayButton', () => {
+  const date = new Date('2022-05-18');
+  const activeMonthYear = new Date(2022, 5);
+  it('should render successfully', () => {
+    const { baseElement } = render(<DayButton date={date} monthYear={activeMonthYear} onChange={() => {}} />);
+    expect(baseElement).toBeTruthy();
+  });
+  it('should make DayButton unavailable if unavailableFrom is before date', () => {
+    const { baseElement } = render(
+      <DayButton date={date} monthYear={activeMonthYear} onChange={() => {}} unavailableFrom="2022-05-17" />
+    );
+    expect(baseElement.getElementsByClassName('is-unavailable').length).toBe(1);
+  });
+  it('should make DayButton unavailable if unavailableTo is after date', () => {
+    const { baseElement } = render(
+      <DayButton date={date} monthYear={activeMonthYear} onChange={() => {}} unavailableTo="2022-05-19" />
+    );
+    expect(baseElement.getElementsByClassName('is-unavailable').length).toBe(1);
+  });
+  it('should make DayButton unavailable if unavailable contains date', () => {
+    const { baseElement } = render(
+      <DayButton date={date} monthYear={activeMonthYear} onChange={() => {}} unavailable={['2022-05-18']} />
+    );
+    expect(baseElement.getElementsByClassName('is-unavailable').length).toBe(1);
+  });
+  it('should make DayButton unavailable if unavailableFrom is same day as date', () => {
+    const { baseElement } = render(
+      <DayButton date={date} monthYear={activeMonthYear} onChange={() => {}} unavailableFrom="2022-05-18" />
+    );
+    expect(baseElement.getElementsByClassName('is-unavailable').length).toBe(1);
+  });
+  it('should make DayButton unavailable if unavailableTo is same day as date', () => {
+    const { baseElement } = render(
+      <DayButton date={date} monthYear={activeMonthYear} onChange={() => {}} unavailableTo="2022-05-18" />
+    );
+    expect(baseElement.getElementsByClassName('is-unavailable').length).toBe(1);
+  });
+  it('should be selected if value is same as date', () => {
+    const { baseElement } = render(
+      <DayButton date={date} value={date} monthYear={activeMonthYear} onChange={() => {}} />
+    );
+    expect(baseElement.getElementsByClassName('is-selected').length).toBe(1);
+  });
+  it('should call onChange', () => {
+    const mockOnChange = jest.fn();
+    const { baseElement } = render(<DayButton date={date} monthYear={activeMonthYear} onChange={mockOnChange} />);
+    const button = baseElement.querySelector('button') as HTMLButtonElement;
+    fireEvent.click(button);
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+});
+
+describe('UI Components - Molecules - Calendar - MonthsView', () => {
+  const activeYear = 2023;
+  it('should render successfully', () => {
+    const { baseElement } = render(<MonthsView activeYear={activeYear} onChange={() => {}} />);
+    expect(baseElement).toBeTruthy();
+  });
+  it('should call onChange', () => {
+    const mockOnChange = jest.fn();
+    const { baseElement } = render(<MonthsView activeYear={activeYear} onChange={mockOnChange} />);
+    const button = baseElement.querySelector('button') as HTMLButtonElement;
+    fireEvent.click(button);
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+  it('should have a selected month if month and year of value correspond to a month in activeYear', () => {
+    const { baseElement } = render(
+      <MonthsView value={new Date(2023, 5)} activeYear={activeYear} onChange={() => {}} />
+    );
+    expect(baseElement.getElementsByClassName('is-selected').length).toBe(1);
+  });
+});
+
+describe('UI Components - Molecules - Calendar - YearsView', () => {
+  const activeMonth = 5;
+  const yearsRowsStart = 2016;
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <YearsView activeMonth={activeMonth} yearsRowsStart={yearsRowsStart} onChange={() => {}} />
+    );
+    expect(baseElement).toBeTruthy();
+  });
+  it('should have a selected year if year of value corresponds to a year in years row', () => {
+    const { baseElement } = render(
+      <YearsView
+        value={new Date(2020, 5)}
+        activeMonth={activeMonth}
+        yearsRowsStart={yearsRowsStart}
+        onChange={() => {}}
+      />
+    );
+    expect(baseElement.getElementsByClassName('is-selected').length).toBe(1);
+  });
+  it('should call onChange', () => {
+    const mockOnChange = jest.fn();
+    const { baseElement } = render(
+      <YearsView activeMonth={activeMonth} yearsRowsStart={yearsRowsStart} onChange={mockOnChange} />
+    );
+    const button = baseElement.querySelector('button') as HTMLButtonElement;
+    fireEvent.click(button);
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+  it('should call onChange with value', () => {
+    const mockOnChange = jest.fn();
+    const { baseElement } = render(
+      <YearsView
+        value={new Date(2020, 5)}
+        activeMonth={activeMonth}
+        yearsRowsStart={yearsRowsStart}
+        onChange={mockOnChange}
+      />
+    );
+    const button = baseElement.querySelector('button') as HTMLButtonElement;
+    fireEvent.click(button);
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+});
