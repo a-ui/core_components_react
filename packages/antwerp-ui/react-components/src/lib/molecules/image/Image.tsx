09289723d7eb1cef08c21a70c41a774e519ffec4
yplarthur <arthur@yapla.be>
2023-03-27 18:44:57 +0200

    ✨ UPCO-24 - Image feedback

--- packages/antwerp-ui/react-components/src/lib/molecules/image/Image.tsx
+++ packages/antwerp-ui/react-components/src/lib/molecules/image/Image.tsx
@@ -1,10 +1,26 @@
+import { ReactNode } from 'react';
 import { classNames } from '../../../utils/dom.utils';
 import { ImageProps } from './Image.types';
+
+type ImageChildren = ReactNode | ReactNode[] | undefined;
 
 export function Image({ alt, children, src, noClass, qa }: ImageProps) {
   const classes = classNames({
     'm-image': !noClass
   });
+
+  const isFigCaptionOrCopyright = (child: ImageChildren) => {
+    const element = child as React.ReactElement;
+    return (
+      child &&
+      typeof child === 'object' &&
+      (element.type === 'figcaption' || (typeof element.type !== 'string' && element.type.name === 'Copyright'))
+    );
+  };
+
+  if (!!children && !isFigCaptionOrCopyright(children)) {
+    console.warn("Image's children should only be a Copyright component or a figcaption element.");
+  }
 
   return (
     <figure className={classes} data-qa={qa}>
