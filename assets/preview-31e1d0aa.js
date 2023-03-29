8987daf992c306db978999f1b9f4401d287d4f1f
NicolasYapla <81327208+NicolasYapla@users.noreply.github.com>
2023-03-29 08:05:36 +0000

    Deploying to gh-pages from  @ 3b4a9cfb5d51bc87e5b4d94534ca68616d0fa37f 🚀

---
+++ assets/preview-31e1d0aa.js
@@ -0,0 +1,2 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},viewMode:"docs"};export{o as parameters};
+//# sourceMappingURL=preview-31e1d0aa.js.map
