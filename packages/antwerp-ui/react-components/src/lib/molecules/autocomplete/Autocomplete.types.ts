551b04deee42d0a030b40f5500bedd1967dd592d
Alec Van Steenbrugge <alec.vansteenbrugge@icapps.com>
2023-08-24 10:02:08 +0200

    added requested changes

--- packages/antwerp-ui/react-components/src/lib/molecules/autocomplete/Autocomplete.types.ts
+++ packages/antwerp-ui/react-components/src/lib/molecules/autocomplete/Autocomplete.types.ts
@@ -8,8 +8,8 @@ export interface AutocompleteProps {
   value?: string | string[];
   noResultsText?: string;
   qa?: string;
-  description?:string;
-  state?:'error'|'success'
+  description?: string;
+  state?: 'error' | 'success';
   onChange?: (value: string | string[], name?: string) => void;
   onInputChange?: (value: string, name?: string) => void;
 }
