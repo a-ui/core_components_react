44b692333498f2e7490cafd44627b09f9107d408
yplarthur <arthur@yapla.be>
2023-08-29 16:27:18 +0200

    🐛 UPCO-170 - Use lazy loading in Modal and Tooltip to resolve Next.js issues for SSR

--- packages/antwerp-ui/react-components/src/lib/atoms/tooltip/Tooltip.tsx
+++ packages/antwerp-ui/react-components/src/lib/atoms/tooltip/Tooltip.tsx
@@ -1,26 +1,29 @@
-import { cloneElement } from 'react';
-import { Tooltip as ReactTooltip } from 'react-tooltip';
+import React, { Suspense, cloneElement } from 'react';
 import 'react-tooltip/dist/react-tooltip.css';
 import { TooltipProps } from './Tooltip.types';
 
 import './Tooltip.css';
 
+const ReactTooltip = React.lazy(() => import('react-tooltip').then((module) => ({ default: module.Tooltip })));
+
 export function Tooltip({ anchor, anchorId, location, text, qa }: TooltipProps) {
   return (
     <>
       {cloneElement(anchor, { id: anchorId, 'aria-describedby': `${anchorId}-tooltip`, tabIndex: 0 })}
-      <ReactTooltip
-        id={`${anchorId}-tooltip`}
-        anchorId={anchorId}
-        className="a-tooltip__base"
-        noArrow
-        positionStrategy="fixed"
-        place={location}
-      >
-        <div className="a-tooltip" data-qa={qa}>
-          {text}
-        </div>
-      </ReactTooltip>
+      <Suspense>
+        <ReactTooltip
+          id={`${anchorId}-tooltip`}
+          anchorId={anchorId}
+          className="a-tooltip__base"
+          noArrow
+          positionStrategy="fixed"
+          place={location}
+        >
+          <div className="a-tooltip" data-qa={qa}>
+            {text}
+          </div>
+        </ReactTooltip>
+      </Suspense>
     </>
   );
 }
--- packages/antwerp-ui/react-components/src/lib/molecules/modal/Modal.tsx
+++ packages/antwerp-ui/react-components/src/lib/molecules/modal/Modal.tsx
@@ -1,9 +1,10 @@
-import React, { cloneElement, MouseEventHandler } from 'react';
-import ReactModal from 'react-modal';
+import React, { cloneElement, MouseEventHandler, Suspense } from 'react';
 import { DEFAULT_SIZE, SIZE_MAP } from '../../../constants/layout.settings';
 import { classNames } from '../../../utils/dom.utils';
 import { Button } from '../../atoms/button';
 import { ModalProps } from './Modal.types';
+
+const ReactModal = React.lazy(() => import('react-modal').then((module) => ({ default: module.default })));
 
 export function Modal({
   appRootId,
@@ -39,57 +40,59 @@ export function Modal({
   return (
     <>
       {trigger ? cloneElement(trigger, { onClick: () => setModalOpen(true) }) : null}
-      <ReactModal
-        appElement={document.getElementById(appRootId) as HTMLElement}
-        bodyOpenClassName={null}
-        htmlOpenClassName={null}
-        className={classNames({ 'm-modal': true, [`m-modal--${SIZE_MAP[size || DEFAULT_SIZE]}`]: !!size })}
-        isOpen={showModal}
-        onRequestClose={handleClose}
-        role="dialog"
-        aria-modal="true"
-        overlayClassName={classNames({
-          'm-overlay': true,
-          'is-active': true
-        })}
-        data-qa={qa}
-        ariaHideApp={true}
-        onAfterOpen={onAfterOpen}
-        shouldCloseOnOverlayClick={cancelButton || confirmButton || closeButton ? shouldCloseOnOverlayClick : true}
-      >
-        <div className="m-modal__content">
-          {(title || closeButton) && (
-            <div className="m-modal__header u-margin-bottom-xs">
-              {title && title?.label && (
-                <HeaderTag className={title.className} id={title.id}>
-                  {title.label}
-                </HeaderTag>
-              )}
-              {closeButton && (
-                <Button
-                  className="m-modal__close"
-                  theme="neutral"
-                  addOn={{ type: 'icon', iconProps: { name: 'ai-close' } }}
-                  ariaLabel={ariaCloseLabel}
-                  emphasis="low"
-                  onClick={handleClose}
-                />
-              )}
-            </div>
-          )}
-          {children && <div className="u-margin-bottom">{children}</div>}
-          {confirmButton || cancelButton ? (
-            <div className="m-modal__footer">
-              {confirmButton ? (
-                <Button {...confirmButton} onClick={(e) => runFunctionAndClose(e, confirmButton.onClick)} />
-              ) : null}
-              {cancelButton ? (
-                <Button {...cancelButton} onClick={(e) => runFunctionAndClose(e, cancelButton.onClick)} />
-              ) : null}
-            </div>
-          ) : null}
-        </div>
-      </ReactModal>
+      <Suspense>
+        <ReactModal
+          appElement={document.getElementById(appRootId) as HTMLElement}
+          bodyOpenClassName={null}
+          htmlOpenClassName={null}
+          className={classNames({ 'm-modal': true, [`m-modal--${SIZE_MAP[size || DEFAULT_SIZE]}`]: !!size })}
+          isOpen={showModal}
+          onRequestClose={handleClose}
+          role="dialog"
+          aria-modal="true"
+          overlayClassName={classNames({
+            'm-overlay': true,
+            'is-active': true
+          })}
+          data-qa={qa}
+          ariaHideApp={true}
+          onAfterOpen={onAfterOpen}
+          shouldCloseOnOverlayClick={cancelButton || confirmButton || closeButton ? shouldCloseOnOverlayClick : true}
+        >
+          <div className="m-modal__content">
+            {(title || closeButton) && (
+              <div className="m-modal__header u-margin-bottom-xs">
+                {title && title?.label && (
+                  <HeaderTag className={title.className} id={title.id}>
+                    {title.label}
+                  </HeaderTag>
+                )}
+                {closeButton && (
+                  <Button
+                    className="m-modal__close"
+                    theme="neutral"
+                    addOn={{ type: 'icon', iconProps: { name: 'ai-close' } }}
+                    ariaLabel={ariaCloseLabel}
+                    emphasis="low"
+                    onClick={handleClose}
+                  />
+                )}
+              </div>
+            )}
+            {children && <div className="u-margin-bottom">{children}</div>}
+            {confirmButton || cancelButton ? (
+              <div className="m-modal__footer">
+                {confirmButton ? (
+                  <Button {...confirmButton} onClick={(e) => runFunctionAndClose(e, confirmButton.onClick)} />
+                ) : null}
+                {cancelButton ? (
+                  <Button {...cancelButton} onClick={(e) => runFunctionAndClose(e, cancelButton.onClick)} />
+                ) : null}
+              </div>
+            ) : null}
+          </div>
+        </ReactModal>
+      </Suspense>
     </>
   );
 }
