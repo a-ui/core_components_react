6386b5d67b099566336a2c456e9434bf3a3cfbfa
yplarthur <arthur@yapla.be>
2023-09-08 09:48:54 +0200

    UPCO-170 - Add SSR compatible components to index.ssr

--- packages/antwerp-ui/react-components/src/index.ssr.ts
+++ packages/antwerp-ui/react-components/src/index.ssr.ts
@@ -1,3 +1,13 @@
 // Components that can be rendered server side with NextJS
+export * from './lib/atoms/avatar';
+export * from './lib/atoms/breadcrumb';
+export * from './lib/atoms/checkbox';
+export * from './lib/atoms/copyright';
 export * from './lib/atoms/label';
+export * from './lib/atoms/progress-bar';
+export * from './lib/atoms/range-slider';
 export * from './lib/atoms/spinner';
+
+export * from './lib/molecules/alert';
+export * from './lib/molecules/card';
+export * from './lib/molecules/image';
