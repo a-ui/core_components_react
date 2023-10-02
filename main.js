c175d7436e8188217d2793073463fcc2fd6ead71
NicolasYapla <81327208+NicolasYapla@users.noreply.github.com>
2023-10-02 12:38:35 +0000

    Deploying to gh-pages from  @ ce9df38983bbeb01aa221eaa8f9b6a032cb8931b 🚀

---
+++ main.js
@@ -0,0 +1,26 @@
+module.exports = {
+  stories: [
+    '../../../packages/antwerp-ui/react-components/src/lib/base/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../packages/antwerp-ui/react-components/src/lib/atoms/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../packages/antwerp-ui/react-components/src/lib/molecules/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../packages/antwerp-ui/react-components/src/lib/organisms/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  features: {
+    storyStoreV7: true
+  },
+  async viteFinal(config) {
+    if (!config?.server) {
+      config.base = '/core_components_react';
+    }
+    return config;
+  },
+  staticDirs: ['.'],
+  docs: {
+    autodocs: true
+  }
+};
