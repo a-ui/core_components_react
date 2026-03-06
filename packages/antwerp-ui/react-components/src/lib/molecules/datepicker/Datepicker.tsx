08821091d2142f5607233e4ef6021007298a6d30
Lukas Olivier <lukas@yapla.be>
2026-03-06 11:58:38 +0100

    Refactor: no fallback should be provided

--- packages/antwerp-ui/react-components/src/lib/molecules/datepicker/Datepicker.tsx
+++ packages/antwerp-ui/react-components/src/lib/molecules/datepicker/Datepicker.tsx
@@ -165,27 +165,25 @@ export function Datepicker({
     }
   };
 
-  const inputId = inputProps?.id ?? 'aui-text-field';
-
   return (
     <div className="a-input has-icon-right" data-qa={qa}>
-      {(label || inputProps?.label) && renderLabel({ label: label || inputProps?.label, id: inputId, required })}
+      {(label || inputProps?.label) && renderLabel({ label: label || inputProps?.label, id: inputProps?.id, required })}
       {renderDescription({
-        id: inputId,
+        id: inputProps?.id,
         description: dateInvalidError || inputProps?.description,
         state: dateInvalidError ? 'error' : inputProps?.state
       })}
       <div className="a-input__wrapper">
         <TextField
           {...inputProps}
-          id={inputId}
+          id={inputProps?.id}
           label={undefined}
           description={undefined}
           type="text"
           value={inputProps.value || formattedValue}
           onChange={handleChange}
           state={dateInvalidError ? 'error' : inputProps?.state}
-          aria-describedby={`${inputId}--description`}
+          aria-describedby={`${inputProps?.id}--description`}
         />
         <Icon
           tabIndex={0}
